a0adf123abed074a16d0dab8c0479abd8bfe01a9
H3NRY33 <37905046+H3NRY33@users.noreply.github.com>
2018-09-17 00:07:01 -0400

    Create HOURS_WASTED.cpp

---
+++ HOURS_WASTED.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <iostream>
+#include <cstdio>
+using namespace std; 
+
+
+/* This program would tell you how many hours of your life you have spent playing video games  */
+//UNFINISHED
+
+
+//function that computes the time period the user has played...	
+float ExPFunc () {
+	
+	 vector<string> time;
+	 float timeLength;
+	 time.push_back("Days");
+	 time.push_back("Weeks");
+	 time.push_back("Months");
+	 time.push_back("Years");
+	printf("%s /n ", " What best describes the amount of time you have played?");
+	
+	for (int i = 0 ; i < time.size(); i++){
+		cout << time[i]<<" ? /n";	
+	}
+	printf("%s /n"," How many")
+	
+	
+	
+}
+int main(int argc, char const *argv[])
+{
+	//Declares valuable variables for following computations
+	
+	//Age of user // Amount of time played//
+	float Age, ExP;
+
+	//Stores the final amount of hours wasted 
+	float WasteHour;
+
+
+
+	
+
+
+
+
+
+	                                                                                                            
+	return 0;
+}
