83c12a47eae6dbcd028e5dcbd60155c434721095
H3NRY33 <37905046+H3NRY33@users.noreply.github.com>
2018-09-20 15:08:14 -0400

    ADDED RELACED

--- RANDOM_PASS.cpp
+++ RANDOM_PASS.cpp
@@ -16,7 +16,6 @@ string to_upper(string &upper){
 	return upper;	
 }
 
-
 int main(int argc, char const *argv[]){
 srand(time(0));
 bool newPass = true;
@@ -50,55 +49,57 @@ cout<<"	3 ------ UPPERCASE and numbers"<<endl;
 
 cout<<"Your new password is: \n\n";
 
-if (option == 1){
+switch(option){
+	case 1:{
 	for(size_t i =0 ; i<length;i++){
 	passI = rand() % 33+93;
 	passC= (char)passI;
 	cout<<passC;
+	}	
+		break;
 	}
-}
-
-if(option == 2){
-
+	case 2:{
 	for(size_t k = 0 ; k<length ; k++){
 	anotherONE:passI = rand() % 48+74;
-	
-	if(passI>=58 && passI<=96){
-	goto anotherONE;
+		if(passI>=58 && passI<=96){
+		goto anotherONE;
 		}
 
-	if(passI >=48 && passI<=57){
-	passC= (char)passI;
-	cout<<passC;
+		if(passI >=48 && passI<=57){
+		passC= (char)passI;
+		cout<<passC;
 		}
-	if (passI >=97 && passI<=122){
-	passC= (char)passI;
-	cout<<passC;
+		if (passI >=97 && passI<=122){
+		passC= (char)passI;
+		cout<<passC;
 		}
+	}	
+		break;
 	}
-
-}
-
-if (option == 3){
+	case 3:{
 	for(size_t j =0 ; j<length;j++){
 	tryagain:passI = rand() % 48+42;
 	
-	if(passI>=58 && passI<=64){
-	goto tryagain;
-	}
-
-	if(passI >=48 && passI<=57){
-	passC= (char)passI;
-	cout<<passC;
+		if(passI>=58 && passI<=64){
+		goto tryagain;
 		}
-	if (passI >=65 && passI<=90){
-	passC= (char)passI;
-	cout<<passC;
+
+		if(passI >=48 && passI<=57){
+		passC= (char)passI;
+		cout<<passC;
+		}
+		if (passI >=65 && passI<=90){
+		passC= (char)passI;
+		cout<<passC;
 		}
 	}
-
+		break;
+	}
+	default:{
+		cout<<"Something went wrong :("<<endl;
+		break;
+	}
 }
-
 cout <<"\n\nWould you like to generate another password?"<<endl;
 cin>>response;
 to_upper(response);
@@ -113,8 +114,4 @@ cout<<"See you next time......."<<endl;
 system("pause");
 return 0;
 
-
 }
-
-
-
